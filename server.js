7ed357502865cce781b65c66bbeb12bd0ee684b0
Christopher Langner <langnerc@icloud.com>
2022-05-15 07:20:39 -0500

    added db query

--- server.js
+++ server.js
@@ -1,19 +1,33 @@
-const express = require('express');
-const res = require('express/lib/response');
+const mysql = require("mysql2");
+const express = require("express");
+const res = require("express/lib/response");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 //middleware
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+//connect to DB
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    //mysql UN
+    user: "root",
+    password: "Nicholas21#",
+    database: "election",
+  },
+  console.log("Connected to the election database")
+);
 
-
-app.use((req,res) => {
-    res.status(404).end();
+db.query(` SELECT * FROM candidates`, (err, rows) => {
+  console.log(rows);
 });
 
+app.use((req, res) => {
+  res.status(404).end();
+});
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
