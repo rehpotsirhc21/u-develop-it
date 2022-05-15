c7ed4bef6284ade55ccef8c28a74ce4f930c8293
Christopher Langner <langnerc@icloud.com>
2022-05-15 11:04:40 -0500

    added votes table

--- db/schema.sql
+++ db/schema.sql
@@ -1,14 +1,16 @@
+DROP TABLE IF EXISTS votes;
+
 DROP TABLE IF EXISTS candidates;
+
 DROP TABLE IF EXISTS parties;
+
 DROP TABLE IF EXISTS voters;
 
 CREATE TABLE parties (
-    id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(50) NOT NULL,
-    description TEXT
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  description TEXT
 );
-
-
 
 CREATE TABLE candidates (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
@@ -16,7 +18,9 @@ CREATE TABLE candidates (
   last_name VARCHAR(30) NOT NULL,
   party_id INTEGER,
   industry_connected BOOLEAN NOT NULL,
-  CONSTRAINT fk_party FOREIGN KEY (party_id) REFERENCES parties(id) ON DELETE SET NULL
+  CONSTRAINT fk_party FOREIGN KEY (party_id) REFERENCES parties(id) ON DELETE
+  SET
+    NULL
 );
 
 CREATE TABLE voters (
@@ -25,4 +29,14 @@ CREATE TABLE voters (
   last_name VARCHAR(30) NOT NULL,
   email VARCHAR(50) NOT NULL,
   created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE votes (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  voter_id INTEGER NOT NULL,
+  candidate_id INTEGER NOT NULL,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  CONSTRAINT uc_voter UNIQUE (voter_id),
+  CONSTRAINT fk_voter FOREIGN KEY (voter_id) REFERENCES voters(id) ON DELETE CASCADE,
+  CONSTRAINT fk_candidate FOREIGN KEY (candidate_id) REFERENCES candidates(id) ON DELETE CASCADE
 );
